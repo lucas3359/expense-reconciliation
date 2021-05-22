126dbcb9afeb81d626f2824bd0552683b7e2d52b
Lucas Huang <lucashsw@gmail.com>
2021-05-22 21:12:36 +1200

    Add list page and dummy API data

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../static/semantic/dist/semantic.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/list.js
+++ pages/list.js
@@ -1,5 +1,40 @@
+import useSWR from 'swr';
+
 export default function List () {
+    const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+    const {data, error} = useSWR('/api/transactions', fetcher)
+
+    if (error) return <div>Failed to load</div>
+    if (!data) return <div>loading...</div>
+
+    const renderedList = () => {
+        return data.map(row => {
+            return (<tr>
+                <td>{row.DTPOSTED}</td>
+                <td>{row.Desc}</td>
+                <td>{row.TRNAMT}</td>
+                </tr>
+                )
+       });
+    }
+
     return(
-        <div>List Page</div>
+        <div className="ui container">
+
+            <h1>Transaction List</h1>
+
+            <table id='table' className = "ui celled table">
+            <thead>
+                <th>Date</th>
+                <th>Desc</th>
+                <th>$</th>
+            </thead>
+            <tbody>
+                {renderedList()}
+            </tbody>
+            </table>
+
+        </div>
     );
 }
