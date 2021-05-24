e682a5646ffe680904a806ff3b046f3a7771f0a1
Thomas Elliott <t.elliott@auckland.ac.nz>
2021-05-24 20:04:52 +1200

    More cleaning up

--- pages/api/transactions.ts
+++ pages/api/transactions.ts
@@ -1,11 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient, transactions } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 export default async (req : NextApiRequest, res : NextApiResponse) => {
-    const transactions : transactions[] = await prisma.transactions.findMany();
+    const transactions : transactions[] = await prisma.transactions.findMany()
 
-    res.status(200).json(transactions);
+    res.status(200).json(transactions)
   }
   
